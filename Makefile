8fe8d3f25bfa4eb356982073f5cc100f01528d22
John-Mark Gurney <jmg@newcontext.com>
2019-11-06 16:01:50 -0800

    start work on adding COA support.

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 test:
 	(ls std_4_9.json stixinfra.py) | (. ./p/bin/activate && ~/src/eradman-entr-c15b0be493fc/entr sh -c 'python -m coverage run -m unittest stixinfra && python -m coverage report -m --omit=p/\*')
 
+test-coa:
+	(ls *.txt *.json stixcoa.py) | (. ./p/bin/activate && ~/src/eradman-entr-c15b0be493fc/entr sh -c 'python -m coverage run -m unittest stixcoa && python -m coverage report -m --omit=p/\*')
+
 setupenv:
 	virtualenv-2.7 p && \
 	( . ./p/bin/activate && pip install -r requirements.txt)
---
+++ stixcoa.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+from stix2.v21 import CustomObject, CourseOfAction
+from stix2.v21.bundle import Bundle
+from stix2 import parse, properties
+
+import json
+import os.path
+import shutil
+import tempfile
+import unittest
+
+__copyright__ = 'Copyright 2019 New Context Services, Inc.'
+__maintainer__ = 'John-Mark Gurney'
+__email__ = 'jmg@newcontext.com'
+
+class Tests(unittest.TestCase):
+	def setUp(self):
+		# setup temporary directory
+		d = os.path.realpath(tempfile.mkdtemp())
+		self.homedir = os.getcwd()
+		self.basetempdir = d
+		self.tempdir = os.path.join(d, 'subdir')
+		os.mkdir(self.tempdir)
+
+		os.chdir(self.tempdir)
+
+	def tearDown(self):
+		shutil.rmtree(self.basetempdir)
+		self.tempdir = None
+
+	def ff(self, *args):
+		return os.path.join(self.homedir, *args)
+
+	def test_standardsample(self):
+		with open(self.ff('coa_code.txt')) as fp:
+			coacode = fp.read()
+
+		with open(self.ff('std_4_3_template.json')) as fp:
+			jsonb = json.load(fp)
+
+		jsonb['objects'][0]['action_bin'] = coacode.encode('base64')
+
+		# Don't need to reserialize to JSON, but want to make sure
+		# it really gets parsed as JSON
+		bundle = parse(json.dumps(jsonb))
+
+		# make sure the action code was inserted correctly
+		coaobj = bundle.objects[0]
+		self.assertEqual(coacode, coaobj.action_bin.decode('base64'))
+
+		runcoa(coaobj)
